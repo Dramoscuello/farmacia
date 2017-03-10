22eea7bf054d4908f39db76bd590ddfc47072c0d
isiasromero <isiasromer@ddd.com>
2017-03-10 00:38:32 -0500

    cambios en medicamentos en mongo -- cree una db manual e inserte los datos

--- app/controllers/medicamentosController.js
+++ app/controllers/medicamentosController.js
@@ -1,30 +1,30 @@
 var express = require('express'),
   router = express.Router(),
   mongoose = require('mongoose');
-  //Medicamentos = mongoose.model("medicamentos");
+  Medicamentos = mongoose.model("medicamentos");
 
-var meds  = [
+/*var meds  = [
   {medicamento: "Lansoprazol", imagen: "img/lansoprazol.jpg", laboratorio: "MK", precio: "$5000"},
   {medicamento: "Naproxeno", imagen: "img/naproxeno.jpg", laboratorio: "MK", precio: "$5000"},
   {medicamento: "Clindamicina", imagen: "img/clindamicina.jpg", laboratorio: "MK", precio: "$18700"},
   {medicamento: "Pirantel", imagen: "img/pirantel.jpg", laboratorio: "Genfar", precio: "$2000"}
-]
+]*/
 
 module.exports = function (app) {
   app.use('/', router);
 };
 
 router.get('/medicamentos', function (req, res, next) {
-  res.render('medicamentos',{
-    titulo: 'Comprar medicamentos',
-    medicamentos: meds
-  });
+  //res.render('medicamentos',{
+  // titulo: 'Comprar medicamentos',
+    //medicamentos: meds
+  //});
 
-      /*Medicamentos.find(function(err, medicamentos){
+      Medicamentos.find(function(err, medicamentos){
         if(err) return next(err);
         res.render('medicamentos',{
           titulo: 'Comprar medicamentos',
           medicamentos: medicamentos
         });
-      });*/
+      });
 });
